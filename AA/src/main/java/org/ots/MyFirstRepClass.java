1492fd23addea6b9009e5013812c40c8de46a6cd
shubh9060 <137057590+shubh9060@users.noreply.github.com>
2024-12-21 12:52:49 +0530

    my Demo Repo Commited ....

---
+++ AA/src/main/java/org/ots/MyFirstRepClass.java
@@ -0,0 +1,11 @@
+package org.ots;
+
+public class MyFirstRepClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.println("Hello World");
+	}
+
+}
